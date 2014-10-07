e00fe7b160d864f67997d4918ad09d1fdd887c4e
Neil Mitchell <ndmitchell@gmail.com>
2014-10-07 15:05:44 +0100

    Improve the web UI a lot, now has styling

--- Development/Bake/Server/Web.hs
+++ Development/Bake/Server/Web.hs
@@ -19,11 +19,11 @@ web oven _ server = return $ OutputHTML $ unlines $
     ["<h2>Patches</h2>"] ++
     table "No patches submitted" ["Patch","Status"] (map (patch oven server) patches) ++
     ["<h2>Clients</h2>"] ++
-    table "No clients available" ["Name","Running"] (map (client server) clients) ++
+    table "No clients available" ["Name","Running"] (map (client oven server) clients) ++
     suffix
     where
         patches = submitted server
-        clients = nub $ map (pClient . snd) $ pings server
+        clients = sort $ nub $ map (pClient . snd) $ pings server
 
 table :: String -> [String] -> [[String]] -> [String]
 table zero cols [] = ["<p>" ++ zero ++ "</p>"]
@@ -40,13 +40,23 @@ prefix =
     ,"<html>"
     ,"<head>"
     ,"<title>Bake Continuous Integration</title>"
+    ,"<style type='text/css'>"
+    ,"body, td {font-family: sans-serif; font-size: 10pt;}"
+    ,"table {border-collapse: collapse;}"
+    ,"table, td {border: 1px solid #ccc;}"
+    ,"td {padding: 2px; padding-right: 15px;}"
+    ,"thead {font-weight: bold;}"
+    ,"a {text-decoration: none; color: #4183c4;}"
+    ,"a:hover {text-decoration: underline;}"
+    ,".patch {background-color: #eee; font-family: Consolas, monospace; border-radius: 3px;}"
+    ,"</style>"
     ,"</head>"
     ,"<body>"
     ,"<h1>Bake Continuous Integration</h1>"
     ]
 
 suffix =
-    ["<p><a href='https://github.com/ndmitchell/bake'>Copyright Neil Mitchell 2014</a></p>"
+    ["<p id=footer><a href='https://github.com/ndmitchell/bake'>Copyright Neil Mitchell 2014</a></p>"
     ,"</body>"
     ,"</html>"]
 
@@ -55,11 +65,12 @@ tag_ t x = "<" ++ t ++ ">" ++ x ++ "</" ++ t ++ ">"
 
 patch :: Oven State Patch Test -> Server -> (UTCTime, Patch) -> [String]
 patch Oven{..} Server{..} (u, p) =
-    ["Patch " ++ stringyPretty ovenStringyPatch p ++ " from " ++ intercalate ", " [a | (pp,a) <- authors, Just p == pp]
+    ["Patch <a href='?patch=" ++ fromPatch p ++ "'' class=patch>" ++ stringyPretty ovenStringyPatch p ++ "</a> from " ++ intercalate ", " [a | (pp,a) <- authors, Just p == pp]
     ,if p `elem` concatMap (candidatePatches . thd3) updates then "In the main repo"
      else if p `elem` candidatePatches active then
         "Actively being worked on " ++ show (length $ filter fst done) ++
         " out of " ++ show todo ++ " (" ++ show (length $ filter (not . fst) done) ++ " failing)"
+     else if p `elem` maybe [] (map snd) paused then "Paused"
      else "Rejected"
     ]
     where
@@ -73,6 +84,9 @@ patch Oven{..} Server{..} (u, p) =
             | (_,Question{..},Just Answer{..}) <- history
             , p `elem` candidatePatches qCandidate]
 
-client :: Server -> Client -> [String]
-client Server{..} c = [show c, show active]
+client :: Oven State Patch Test -> Server -> Client -> [String]
+client Oven{..} Server{..} c =
+    [fromClient c
+    ,if null active then "<i>None</i>"
+     else intercalate ", " $ map (maybe "Preparing" (stringyPretty ovenStringyTest)) active]
     where active = [qTest | (_,Question{..},Nothing) <- history, qClient == c]
