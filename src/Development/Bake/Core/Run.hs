ec0cbec4a634eb11c1d5fa61184e74d41e0bc8bc
Neil Mitchell <ndmitchell@gmail.com>
2014-12-03 12:26:35 +0000

    Add something for self-running executables

---
+++ src/Development/Bake/Core/Run.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE RecordWildCards, DeriveDataTypeable #-}
+
+module Development.Bake.Core.Run(
+    runInit, runUpdate, runTest, runExtra
+    ) where
+
+import Development.Bake.Core.Type hiding (Client)
+import Development.Bake.Core.Message
+import Development.Shake.Command
+import Control.Exception.Extra
+import General.Str
+import System.Time.Extra
+import Control.DeepSeq
+import Data.Tuple.Extra
+import System.Directory
+import System.IO.Extra
+import System.Environment.Extra
+import System.FilePath
+import Data.Hashable
+import Data.Maybe
+import System.Exit
+
+
+state x = "--state=" ++ fromState x
+patch x = "--patch=" ++ fromPatch x
+test  x = "--test="  ++ fromTest  x
+
+runInit :: IO (Maybe State, Answer)
+runInit = runAll "init" [] [] State
+
+runUpdate :: State -> [Patch] -> IO (Maybe State, Answer)
+runUpdate s ps = runAll "update" (state s : map patch ps) [] State
+
+runTest :: State -> [Patch] -> Maybe Test -> IO Answer
+runTest s ps t = do
+    (ex, ans) <- runAll "test" (state s : map patch ps) (map test $ maybeToList t) (both (map Test) . read)
+    return $ maybe ans (\ex -> ans{aTestsSuitable=ex}) ex
+
+runExtra :: State -> Maybe Patch -> IO ((Str, Str), Answer)
+runExtra s ps = do
+    (ex,ans) <- runAll "extra" (state s : map patch (maybeToList ps)) [] (both strPack . read)
+    return (fromMaybe (dupe $ aStdout ans) ex, ans)
+
+
+runAll :: NFData a => String -> [String] -> [String] -> (String -> a) -> IO (Maybe a, Answer)
+runAll name args1 args2 parse = do
+    exe <- getExecutablePath
+    dir <-
+        if null args1 then do
+            let dir = "bake-" ++ name
+            removeDirectoryRecursive dir
+            return dir
+        else do
+            let dir = "bake-" ++ name ++ show (abs $ hash args1)
+            writeFile (dir <.> "txt") $ unlines args1
+            return dir
+    createDirectoryIfMissing True dir
+
+    (time, res) <- duration $ try_ $ do
+        exe <- getExecutablePath
+        (exit, Stdout sout, Stderr serr) <- cmd (Cwd dir) exe ("run" ++ name) args1 args2
+        ex <- if exit /= ExitSuccess then return Nothing else do
+            ans <- fmap parse $ readFile' $ dir </> ".bake"
+            evaluate $ rnf ans
+            return $ Just ans
+        return (ex, Answer (strPack $ sout++serr) 0 ([],[]) (exit == ExitSuccess))
+    case res of
+        Left e -> do
+            e <- showException e
+            return (Nothing, Answer (strPack e) time ([],[]) False)
+        Right (ex,ans) -> return (ex, ans{aDuration=time})
