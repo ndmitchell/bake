a3779d93ed742b8b8012121aa2cab993de3919d3
Neil Mitchell <ndmitchell@gmail.com>
2014-12-15 07:32:58 +0000

    Don't update the patch state to move someone to the top, since it damages all the information you already have

--- src/Development/Bake/Server/Start.hs
+++ src/Development/Bake/Server/Start.hs
@@ -90,10 +90,13 @@ operate timeout oven message server = case message of
         if p `elem` concatMap (maybe [] snd . thd3) (updates server) then
             -- gets confusing if a patch is both included AND active
             dull server
+         else if p `elem` snd (target server) then
+            -- moving a promotion requires retrying every previous promotion
+            dull server
          else
             dull server
                 {target = second (if isJust (paused server) then id else add) $ target server
-                ,paused = (if p `elem` snd (target server) then id else add) <$> paused server
+                ,paused = add <$> paused server
                 ,authors = Map.insertWith (++) (Just p) [author] $ authors server
                 ,submitted = (now,p) : submitted server}
     DelPatch author p -> do
