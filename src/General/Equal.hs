e4b6104bc11f7deccc166b3568b9080c35b47ddb
Neil Mitchell <ndmitchell@gmail.com>
2015-02-21 21:29:49 +0000

    Give Ord and Show for Equal

--- src/General/Equal.hs
+++ src/General/Equal.hs
@@ -12,6 +12,9 @@ import Data.Hashable
 
 data Equal a = Equal (IORef Unique) Int a
 
+instance Show a => Show (Equal a) where
+    show = show . fromEqual
+
 fromEqual :: Equal a -> a
 fromEqual (Equal _ _ x) = x
 
@@ -29,3 +32,14 @@ instance Eq a => Eq (Equal a) where
              else do
                 if uu1 > uu2 then writeIORef u1 uu2 else writeIORef u2 uu1
                 return True
+
+instance Ord a => Ord (Equal a) where
+    compare (Equal u1 h1 x1) (Equal u2 h2 x2)
+        | h1 /= h2 = compare h1 h2
+        | otherwise = unsafePerformIO $ do
+            uu1 <- readIORef u1
+            uu2 <- readIORef u2
+            case compare x1 x2 of
+                _ | uu1 == uu2 -> return EQ
+                EQ -> do if uu1 > uu2 then writeIORef u1 uu2 else writeIORef u2 uu1; return EQ
+                x -> return x
