a3fc789e2136c5d2833cdc3ca87786736f4bcfb5
Neil Mitchell <ndmitchell@gmail.com>
2015-02-23 15:09:03 +0000

    Move the bits of Query required into Brains

--- src/Development/Bake/Server/Brains.hs
+++ src/Development/Bake/Server/Brains.hs
@@ -7,7 +7,6 @@ module Development.Bake.Server.Brains(
 import Development.Bake.Core.Message
 import Development.Bake.Core.Type
 import Development.Bake.Server.Type
-import Development.Bake.Server.Query(asked, test', candidateExact', answered, success', client')
 import Control.DeepSeq
 import Data.Maybe
 import Data.Monoid
@@ -158,3 +157,34 @@ translate :: Server -> State -> (State, [Patch]) -> Maybe [Patch]
 translate server s1 (s2,p2) = stripPrefix pp1 pp2
     where (_,pp1,pp2) = normalise server (s1,[]) (s2,p2)
 
+
+---------------------------------------------------------------------
+-- QUERY HELPERS
+
+type Query = Server -> Question -> Maybe Answer -> Bool
+
+(&&^) :: Query -> Query -> Query
+(&&^) f g s q a = f s q a && g s q a
+
+true' :: Query
+true' _ _ _ = True
+
+
+asked :: Server -> [Query] -> [(Question, Maybe Answer)]
+asked server qs = [(q, a) | (_, q, a) <- history server, pred q a]
+    where pred = foldl (&&^) true' qs server
+
+answered :: Server -> [Query] -> [(Question, Answer)]
+answered server query = [(q, a) | (q, Just a) <- asked server query]
+
+success' :: Query
+success' _ _ = maybe False aSuccess
+
+client' :: Client -> Query
+client' c _ q _ = qClient q == c
+
+test' :: Maybe Test -> Query
+test' t _ q _ = qTest q == t
+
+candidateExact' :: (State, [Patch]) -> Query
+candidateExact' c _ q _ = qCandidate q == c
