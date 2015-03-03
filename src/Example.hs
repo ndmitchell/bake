c208f216b562da8db36062bace7a737503724805
Neil Mitchell <ndmitchell@gmail.com>
2015-03-03 16:16:44 +0000

    Change the example to use ovenStepGit rather than ovenIncremental

--- src/Example.hs
+++ src/Example.hs
@@ -6,9 +6,14 @@ import Development.Shake.Command
 import System.Environment.Extra
 import System.FilePath
 import Data.List.Extra
+import System.Directory
 import Control.Arrow
+import Control.Monad
 import Data.Maybe
 import System.Time.Extra
+
+-- if True then use ovenStep, otherwise use ovenIncremental
+useStep = True
 
 
 data Platform = Linux | Windows deriving (Show,Read)
@@ -21,9 +26,9 @@ main = do
     let err = "You need to set an environment variable named $REPO for the Git repo"
     repo <- fromMaybe (error err) `fmap` lookupEnv "REPO"
     bake $
-        ovenIncremental $
+        (if useStep then id else ovenIncremental) $
         ovenPretty "=" $
-        ovenGit repo "master" Nothing $
+        ((if useStep then ovenStepGit compile else ovenGit) repo "master" Nothing) $
         ovenNotifyStdout $
         ovenTest testStringy (return allTests) execute
         defaultOven{ovenServer=("127.0.0.1",5000)}
@@ -34,15 +39,26 @@ testStringy = Stringy shw rd shw
 
 allTests = [(p,t) | p <- platforms, t <- Compile : map Run [1,10,0]]
 
+compile :: IO [FilePath]
+compile = do
+    createDirectoryIfMissing True "dist"
+    unit $ cmd "ghc --make Main.hs -o dist/Main"
+    sleep 1
+    return ["dist"]
+
 execute :: (Platform,Action) -> TestInfo (Platform,Action)
-execute (p,Compile) = matchOS p $ run $ do
+execute (p,Compile) = matchOS p $ run $ unless useStep $ do
     () <- cmd "ghc --make Main.hs"
     -- ghc --make only has 1 second timestamp resolution
     -- so sleep for a second to make sure we work with incremental
     sleep 1
     incrementalDone
-execute (p,Run i) = require [(p,Compile)] $ matchOS p $ run $ do
-    cmd ("." </> "Main") (show i)
+execute (p,Run i) = require [(p,Compile)] $ matchOS p $ run $
+    if useStep then do
+        [dist] <- stepGet
+        cmd (dist </> "Main") (show i)
+    else
+        cmd ("." </> "Main") (show i)
 
 -- So we can run both clients on one platform we use an environment variable
 -- to fake changing OS
