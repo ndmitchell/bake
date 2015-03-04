48eb9e77e9706be51da7fc0dfe50babccc2b5028
Neil Mitchell <ndmitchell@gmail.com>
2015-03-04 15:45:38 +0000

    Increase the number of retries in the test, slower and less parallel if you use the step method

--- src/Test.hs
+++ src/Test.hs
@@ -96,7 +96,7 @@ test dir = do
         edit "tony" $
             writeFile "Main.hs" "module Main where\n\n-- Entry point\nmain :: IO ()\nmain = print 1\n"
 
-        retry 5 $ do
+        retry 10 $ do
             sleep 10
             withTempDir $ \d -> withCurrentDirectory d $ do
                 unit $ cmd "git clone" repo "."
@@ -120,7 +120,7 @@ test dir = do
             unit $ cmd "git merge origin/master"
             writeFile "Main.hs" "-- Tony waz ere\nmodule Main(main) where\n\n-- Entry point\nmain :: IO ()\nmain = print 1\n"
 
-        retry 10 $ do
+        retry 15 $ do
             sleep 10
             withTempDir $ \d -> withCurrentDirectory d $ do
                 unit $ cmd "git clone" repo "."
