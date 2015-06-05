5013f9c5cffc0b9df1b0d06886d4d79943d23e66
Neil Mitchell <ndmitchell@gmail.com>
2015-06-05 11:04:25 +0100

    Take a way to resolve email addresses

--- src/Development/Bake/Email.hs
+++ src/Development/Bake/Email.hs
@@ -7,11 +7,10 @@ import Data.String
 
 
 -- | Email notifications when users should be notified about success/failure.
---   Requires the host/port of an SMTP server. Not yet implemented, will always crash.
-ovenNotifyEmail :: (Host,Port) -> Oven state patch test -> Oven state patch test
-ovenNotifyEmail hp o = o{ovenNotify = \a s -> sendEmail hp a s >> ovenNotify o a s}
+ovenNotifyEmail :: (Host,Port) -> (Author -> [String]) -> Oven state patch test -> Oven state patch test
+ovenNotifyEmail hp resolve o = o{ovenNotify = \a s -> sendEmail hp (concatMap resolve a) s >> ovenNotify o a s}
 
-sendEmail :: (Host,Port) -> [Author] -> String -> IO ()
+sendEmail :: (Host,Port) -> [String] -> String -> IO ()
 sendEmail (h,p) to msg = sendMail' h (fromIntegral p) email
     where email = simpleMail (addr "bake@example.com") (map addr to) [] [] (fromString "Bake notification") [plainTextPart $ fromString msg]
           addr x = Address Nothing (fromString x)
