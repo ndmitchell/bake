3611f5a8e1a29d7a02ae43fe5d24832856165a9f
Neil Mitchell <ndmitchell@gmail.com>
2015-06-05 11:05:02 +0100

    Dedupe email addresses

--- src/Development/Bake/Email.hs
+++ src/Development/Bake/Email.hs
@@ -4,11 +4,12 @@ module Development.Bake.Email(ovenNotifyEmail) where
 import Development.Bake.Core.Type
 import Network.Mail.SMTP
 import Data.String
+import Data.List.Extra
 
 
 -- | Email notifications when users should be notified about success/failure.
 ovenNotifyEmail :: (Host,Port) -> (Author -> [String]) -> Oven state patch test -> Oven state patch test
-ovenNotifyEmail hp resolve o = o{ovenNotify = \a s -> sendEmail hp (concatMap resolve a) s >> ovenNotify o a s}
+ovenNotifyEmail hp resolve o = o{ovenNotify = \a s -> sendEmail hp (nubOrd $ concatMap resolve a) s >> ovenNotify o a s}
 
 sendEmail :: (Host,Port) -> [String] -> String -> IO ()
 sendEmail (h,p) to msg = sendMail' h (fromIntegral p) email
