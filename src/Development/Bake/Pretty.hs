5d75c652f6c6fa85124242d5a172b1aa81b5bcf5
Neil Mitchell <ndmitchell@gmail.com>
2015-06-29 14:47:42 +0100

    Add some doc comments to Pretty module

--- src/Development/Bake/Pretty.hs
+++ src/Development/Bake/Pretty.hs
@@ -16,6 +16,9 @@ instance Stringy a => Stringy (Pretty a) where
     stringyPretty (Pretty a b) = a ++ "=" ++ stringyPretty b
 
 
+-- | Define an oven that allows @foo=...@ annotations to be added to the strings.
+--   These can be used to annotate important information, e.g. instead of talking about
+--   Git SHA1's, you can talk about @person=SHA1@ or @branch=SHA1@.
 ovenPretty :: Oven state patch test -> Oven state (Pretty patch) test
 ovenPretty oven@Oven{..} = oven
     {ovenUpdate = \s ps -> ovenUpdate s (map unpretty ps)
@@ -27,6 +30,8 @@ ovenPretty oven@Oven{..} = oven
         unpretty :: Pretty a -> a
         unpretty (Pretty _ x) = x
 
+-- | An oven suitable for use with 'ovenPretty' that supersedes patches which have the same
+--   pretty name.
 ovenPrettyMerge :: Oven state (Pretty patch) test -> Oven state (Pretty patch) test
 ovenPrettyMerge oven = oven
     {ovenSupersede = \(Pretty p1 _) (Pretty p2 _) -> p1 == p2
